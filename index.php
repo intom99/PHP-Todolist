37e06f560f679ff51bfb0e4a6a4342b21b4f552f
intom <iin.arahmah99@gmail.com>
2021-08-04 22:38:30 +0700

    update file index

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ $db = mysqli_connect('localhost', 'root', '', 'todolist');
 if (isset($_POST['submit'])) {
     $task = $_POST['task'];
     if (empty($task)) {
-        $err = "You must fill in the task!!";
+        $err = "You must fill in the task!";
     } else {
         mysqli_query($db, "INSERT INTO tasks(task) VALUES ('$task')");
         header('location: index.php');
@@ -28,7 +28,7 @@ $tasks = mysqli_query($db, "SELECT * FROM tasks");
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style1.css">
     <title>Todo list</title>
 </head>
 
@@ -40,41 +40,43 @@ $tasks = mysqli_query($db, "SELECT * FROM tasks");
             </h2>
         </div>
 
-        <div class="content-input">
-            <form action="index.php" method="POST">
-                <?php if (isset($err)) { ?>
-                    <p><?php echo $err ?></p>
-                <?php } ?>
-                <input type="text" class="" name="task">
-                <button class="btn" name="submit" type="submit">Add Task</button>
-            </form>
-
-            <table>
-                <thead>
-                    <tr>
-                        <th>No</th>
-                        <th>Task</th>
-                        <th>Action</th>
-                    </tr>
-                </thead>
-
-                <tbody>
-                    <?php $i = 1;
-                    while ($row = mysqli_fetch_array($tasks)) { ?>
-
-                        <tr>
-                            <td><?php echo $i++; ?></td>
-                            <td class="task"><?php echo $row['task']; ?></td>
-                            <td class="delete">
-                                <a href="index.php?del_task=<?php echo $row['id'] ?>">x</a>
-                            </td>
-                        </tr>
+        <div class="content">
+            <div class="content-input">
+                <form action="index.php" method="POST">
+                    <?php if (isset($err)) { ?>
+                        <p><?php echo $err ?></p>
                     <?php } ?>
-                </tbody>
-            </table>
+                    <textarea name="task" id="" cols="30" rows="5"></textarea>
+                    <button class="btn" name="submit" type="submit">Add Task</button>
+                </form>
+            </div>
+
+            <div class="content-table">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>No</th>
+                            <th>Task</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        <?php $i = 1;
+                        while ($row = mysqli_fetch_array($tasks)) { ?>
+
+                            <tr>
+                                <td><?php echo $i++; ?></td>
+                                <td class="task"><?php echo $row['task']; ?></td>
+                                <td class="delete">
+                                    <a href="index.php?del_task=<?php echo $row['id'] ?>">x</a>
+                                </td>
+                            </tr>
+                        <?php } ?>
+                    </tbody>
+                </table>
+            </div>
         </div>
-
-
 
     </div>
 </body>
